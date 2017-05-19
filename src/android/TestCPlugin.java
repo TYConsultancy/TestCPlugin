164f715da7bf60f422f35cbe6a344f1de9f94e37
Yequiz Test <yequiztest@gmail.com>
2017-05-19 17:28:36 -0500

    Initial commit

---
+++ src/android/TestCPlugin.java
@@ -0,0 +1,32 @@
+package com.cordovatest.plugins;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class TestCPlugin extends CordovaPlugin {
+
+  @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("greet")) {
+
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+}
+}
